bb8b7e14ed44e2b306af4dbd57f9965463288350
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-07-24 17:03:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
-// Your script here.
+const dropdown = document.getElementById("voices");
+
+const rate = document.getElementById("rate");
+const pitch = document.getElementById("pitch");
+
+const stop = document.getElementById("stop");
+const speak = document.getElementById("speak");
+
+const textarea = document.getElementById("textarea");
+
+
+let speechSynthesis = window.speechSynthesis;
+
+let utterance = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+
+console.log(speechSynthesis);
+console.log(utterance);
+
+
+stop.addEventListener("click",function(){
+    speechSynthesis.cancel();
+})
+
+speak.addEventListener("click", function(){
+    if(speechSynthesis.speaking === true){
+        alert("already speaking");
+    }else if(textarea.value !== ""){
+        utterance.text = textarea.value;
+        utterance.pitch = pitch.value;
+        utterance.rate = rate.value;
+        speechSynthesis.speak(utterance);
+
+    }
+})
+
+
+pitch.addEventListener("change", function(){
+    utterance.pitch = pitch.value;
+})
+
+rate.addEventListener("change", function(){
+    utterance.rate = rate.value;
+})
+
+
+dropdown.addEventListener("change", function(){
+    let selected = dropdown.value;
+
+    utterance.voice = selected;
+})
+
+
+function getAllVoices(){
+    let receivedVoice = speechSynthesis.getVoices();
+
+    receivedVoice.map((eachVoice) => {
+        let eachVoiceName = eachVoice.name;
+        let eachVoiceLang = eachVoice.lang;
+
+        let option = document.createElement("option");
+        option.setAttribute("value", `${eachVoiceName}`);
+
+        option.innerText = `${eachVoiceName} (${eachVoiceLang})`;
+
+        dropdown.appendChild(option);
+
+        
+    })
+}
+
+speechSynthesis.onvoiceschanged = getAllVoices;
